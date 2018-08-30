0c6d1e10ba03be0bc836089de98fab604ac54da7
Ada 2018 <>
2018-08-30 17:42:39 -0300

    Adding tableview to register view

---
+++ ArretadasFGC/ArretadasFGC/New Group/CardRegisterAccountForm.swift
@@ -0,0 +1,21 @@
+//
+//  CardRegisterAccountForm.swift
+//  ArretadasFGC
+//
+//  Created by Ada 2018 on 30/08/2018.
+//  Copyright © 2018 Ada 2018. All rights reserved.
+//
+
+import UIKit
+
+class CardRegisterAccountForm: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
