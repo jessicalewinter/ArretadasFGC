e9cdf091c79c11ae4620e387790c5a85fc89f5e8
Paloma Bispo <palomabispo@alu.ufc.br>
2018-08-31 08:09:41 -0300

    updates profile user

--- ArretadasFGC/ArretadasFGC/Controllers/ProfileUserViewController.swift
+++ ArretadasFGC/ArretadasFGC/Controllers/ProfileUserViewController.swift
@@ -11,40 +11,76 @@ import CoreData
 
 class ProfileUserViewController: UIViewController {
 
+    @IBOutlet var stackRoot: UIStackView!
     @IBOutlet var tableView: UITableView!
     
     var user: User?
     var icons = ["briefcase", "comment", "location"]
     var info: [String] = []
+    private lazy var backgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 10.0
+        view.layer.shadowColor = #colorLiteral(red: 0.4390000105, green: 0.4390000105, blue: 0.4390000105, alpha: 1)
+        view.layer.shadowRadius = 1
+        view.layer.shadowOpacity = 0.3
+        view.layer.shadowOffset = CGSize.init(width: 3.0, height: 5.0)
+        view.clipsToBounds = false
+        view.layer.masksToBounds = false
+        
+        return view
+    }()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
         info = [user?.city ?? "Não Informado", user?.profession ?? "Não Informado", user?.bio ?? "Não Informado" ]
         setupLayout()
 	}
     
-    func setupLayout(){
-        tableView.dataSource = self
-        tableView.delegate = self
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        sizeHeader()
+        setupLayout()
+    }
+    
+    
+    func sizeHeader(){
+        guard let headerView = tableView.tableHeaderView else {
+            return
+        }
+        headerView.setNeedsLayout()
+        headerView.layoutIfNeeded()
+        var frame = headerView.frame
+        frame.size.height = self.view.frame.size.height*0.53
+        headerView.frame = frame
         
-        tableView.clipsToBounds = true
-        tableView.layer.cornerRadius = 10
+        tableView.tableHeaderView = headerView
+    }
+    
+    func setupLayout(){
+//        tableView.dataSource = self
+//        tableView.delegate = self
+//
+//        tableView.clipsToBounds = true
+//        tableView.layer.cornerRadius = 10
+        pinBackground(backgroundView, to: stackRoot)
+        
+        
+    }
+	
+    private func pinBackground(_ view: UIView, to stackView: UIStackView) {
+        view.translatesAutoresizingMaskIntoConstraints = false
+        stackView.insertSubview(view, at: 0)
+        view.pin(to: stackView)
     }
 	
 	
-	
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
 }
 
 extension ProfileUserViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return icons.count
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellProfileUser", for: indexPath)
         cell.imageView?.image = UIImage(named: icons[indexPath.row])
@@ -53,5 +89,54 @@ extension ProfileUserViewController: UITableViewDelegate, UITableViewDataSource{
     }
     
     
+}
+
+extension UIView {
+    func addBorder(toEdges edges: UIRectEdge, color: UIColor, thickness: CGFloat) {
+        
+        func addBorder(toEdge edges: UIRectEdge, color: UIColor, thickness: CGFloat) {
+            let border = CALayer()
+            border.backgroundColor = color.cgColor
+            
+            switch edges {
+            case .top:
+                border.frame = CGRect(x: 0, y: 0, width: frame.width, height: thickness)
+            case .bottom:
+                border.frame = CGRect(x: 0, y: frame.height - thickness, width: frame.width, height: thickness)
+            case .left:
+                border.frame = CGRect(x: 0, y: 0, width: thickness, height: frame.height)
+            case .right:
+                border.frame = CGRect(x: frame.width - thickness, y: 0, width: thickness, height: frame.height)
+            default:
+                break
+            }
+            
+            layer.addSublayer(border)
+        }
+        
+        if edges.contains(.top) || edges.contains(.all) {
+            addBorder(toEdge: .top, color: color, thickness: thickness)
+        }
+        
+        if edges.contains(.bottom) || edges.contains(.all) {
+            addBorder(toEdge: .bottom, color: color, thickness: thickness)
+        }
+        
+        if edges.contains(.left) || edges.contains(.all) {
+            addBorder(toEdge: .left, color: color, thickness: thickness)
+        }
+        
+        if edges.contains(.right) || edges.contains(.all) {
+            addBorder(toEdge: .right, color: color, thickness: thickness)
+        }
+    }
+    public func pin(to view: UIView) {
+        NSLayoutConstraint.activate([
+            leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            topAnchor.constraint(equalTo: view.topAnchor),
+            bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            ])
+    }
 }
 
---
+++ ArretadasFGC/ArretadasFGC/Xibs/ClubsCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ClubsCollectionViewCell.swift
+//  ArretadasFGC
+//
+//  Created by Ada 2018 on 30/08/2018.
+//  Copyright © 2018 Ada 2018. All rights reserved.
+//
+
+import UIKit
+
+class ClubsCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet var image: UIImageView!
+    @IBOutlet var labelName: UILabel!
+    @IBOutlet var iconLocal: UIImageView!
+    @IBOutlet var labelLocal: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
