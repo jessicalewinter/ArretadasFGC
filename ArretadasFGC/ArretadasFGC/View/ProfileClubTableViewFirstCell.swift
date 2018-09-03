3d7acb160a74738a7e8dae546a6d368bb04b10c7
Débora Moura <deboramour4@gmail.com>
2018-09-03 10:27:17 -0300

    Profile club

---
+++ ArretadasFGC/ArretadasFGC/View/ProfileClubTableViewFirstCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileClubTableViewFirstCell.swift
+//  ArretadasFGC
+//
+//  Created by Ada 2018 on 03/09/18.
+//  Copyright © 2018 Ada 2018. All rights reserved.
+//
+
+import UIKit
+
+class ProfileClubTableViewFirstCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
